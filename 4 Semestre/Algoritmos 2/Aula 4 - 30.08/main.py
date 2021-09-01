9380dd9d1f833a2cad0bbd71f2d2c1a541b85a80
Jonatha Garcia <jonathafgarcia@gmail.com>
2021-08-31 22:30:48 -0300

    progweb aula 4

---
+++ 4 Semestre/Algoritmos 2/Aula 4 - 30.08/main.py
@@ -0,0 +1,7 @@
+from produto import Produto
+
+p1 = Produto("Pepsi", 4.99)
+p2 = Produto("Coca-Cola", 5.99)
+p3 = Produto("Fruki", 3.99)
+p4 = Produto()
+
---
+++ 4 Semestre/Algoritmos 2/Aula 4 - 30.08/produto.py
@@ -0,0 +1,4 @@
+class Produto:
+    def __init__(self, nome = None, preco = 0.0):
+        self.nome = nome
+        self.preco = preco
